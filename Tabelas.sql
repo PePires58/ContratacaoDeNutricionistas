b61e8be8c0f0f123cf1dfbf9928f6da3824c5e44
tatianelucia <41492146+tatianelucia@users.noreply.github.com>
2020-05-30 14:19:03 -0300

    Add files via upload

--- Tabelas.sql
+++ Tabelas.sql
@@ -5,8 +5,7 @@ Use ContratacaoDeNutricionistas
 /* 
 EXCLUIR TABELAS:
  
-Drop Table ficha_tb
-Drop Table consulta_tb
+Drop Table usuario_tb
 Drop Table agenda_tb
 Drop Table endereco_tb
 Drop Table paciente_tb
@@ -14,95 +13,46 @@ Drop Table nutricionista_tb
 
 */
 
---Tudo em uma tabela:
-/*
+--Tabela usuario
 Create Table usuario_tb (
-	id_usuario				Int Identity(1,1) Primary Key	Not Null, --Identificador de cada usuário(chave primaria).
-	CPF_CNPJ				Varchar(14)						Not Null, --CNPJ OU CPF do usuario.						 
-	CRM						Int Null						Null, CRM do nutricionista.
-	Nome					Varchar(50)						Not Null, --Nome do usuário
-	Telefone				Varchar(12)						Not Null, --Telefone para contato.
-	tp_usuario				Char(1)							Not Null, -- F - FEMININO ou M - MASCULINO
-	Avaliacao				Float Null					    Null, --Acho que deveria ser Numeric(2,2)
-	Login					Varchar(20)						Not Null, --Para o acesso.
-	senha					Varchar(8)						Not Null --Senha para o acesso.
-)
-*/
-
---Forma individual - Inicio
---Criação da tabela do Paciente
-Create Table paciente_tb (
-	id_paciente				Int Identity(1,1) Primary Key	Not Null, --Identificação única do paciente(chave primaria).
-	CPF_CNPJ				Varchar(14)						Not Null, --CNPJ OU CPF do paciente.
-	Nome					Varchar(50)						Not Null, --Nome do paciente.
-	Telefone				Varchar(12)						Not Null, --Telefone para contato do paciente.
-	Sexo					Char(1)							Not Null, -- F - FEMININO ou M - MASCULINO
-	Login					Varchar(20)						Not Null, --Para o acesso.
-	senha					Varchar(20)						Not Null --Senha para o acesso.
+	id_usuario				INT Identity(1,1) Primary Key	Not Null, --Identificador de cada usuário(chave primaria).
+	CPF						VARCHAR(14)						Not Null, --CPF do usuario.						 
+	CRM						INT Null						Null,	  --CRM do nutricionista.
+	Nome					VARCHAR(50)						Not Null, --Nome do usuário
+	Telefone				VARCHAR(12)						Not Null, --Telefone para contato.
+	tp_usuario				INT								Not Null, --0 - Paciente ou 1 - Nutricionista
+	Avaliacao				FLOAT						    Null,     --Acho que deveria ser Numeric(2,2)
+	Login					VARCHAR(20)						Not Null, --Para o acesso.
+	senha					VARCHAR(8)						Not Null  --Senha para o acesso.
 )
 
---Criação da tabela do Nutricionista
-Create Table nutricionista_tb (
-	id_nutri				Int Identity(1,1) Primary Key	Not Null, --Identificação única do nutricionista(chave primaria).
-	CRM						Varchar(20)						Null, --CRM pode ou não ser obrigatório.
-	CPF_CNPJ				Varchar(14)						Not Null, --CNPJ OU CPF do nutricionista.
-	Nome					Varchar(50)						Not Null, --Nome do nutricionista.
-	Telefone				Varchar(12)						Not Null, --Telefone para contato do nutricionista.
-	Sexo					Char							Not Null, -- F - FEMININO ou M - MASCULINO
-	Avaliacao				Float							Not Null, --Acho que deveria ser Numeric(2,2)
-	Login					Varchar(20)						Not Null, --Para o acesso.
-	senha					Varchar(8)						Not Null --Senha para o acesso.
-)
---Fim
-
---Criação da tabela do Local de Trabalho
+--Tabela endereço do Nutricionista
 Create Table endereco_tb (
-	id_endereco				Int Identity(1,1) Primary Key	Not Null, --Identificação do endereco(acho que não deveria ter)(chave primaria).
-	id_nutri				Int								Not Null, --Id da(o) nutricionista.
-	Rua						Varchar(100)					Not Null, --Nome da rua.
-	Complemento				Varchar(5)						Null, --Se é casa, apto...
-	Numero					Int								Not Null, --Numero da casa, apto..
-	Bairro					Varchar(50)						Not Null, --Nome do bairro.
-	CEP						Varchar(8)						Not Null, --numero do CEP.
-	Constraint FK_id_nutri_nutricionista_tb FOREIGN KEY (id_nutri) References nutricionista_tb (id_nutri) --Chave estrangeira
+	id_endereco				INT Identity(1,1) Primary Key	Not Null, --Identificação do endereco(acho que não deveria ter)(chave primaria).
+	id_nutri				INT								Not Null, --Id da(o) nutricionista.
+	Rua						VARCHAR(100)					Not Null, --Nome da rua.
+	Complemento				VARCHAR(5)						Null,     --Se é casa, apto...
+	Numero					INT								Not Null, --Numero da casa, apto..
+	Bairro					VARCHAR(50)						Not Null, --Nome do bairro.
+	Cidade					VARCHAR(30)						Not Null, --Nome da cidade.
+	Estado					VARCHAR(2)						Not Null, --Sigla do estado.
+	CEP						VARCHAR(8)						Not Null, --numero do CEP.
+	Constraint FK_id_usuario_usuario_tb2 Foreign Key (id_usuario) References usuario_tb (id_usuario), --Chave estrangeira
 )
 
---Criação databela de Agendamentos
+--Tabela de Agendamentos
 Create Table agenda_tb (
-	id_agenda				Int Identity(1,1) Primary Key	Not Null, --Identificar agenda do nutricionista(também acho que não deveria ter).(chave primaria)
-	id_nutri				Int								Not Null, --Id da(o) nutricionista.
-	id_endereco				Int								Not Null, --id do endereço do nutricionista.
+	id_agenda				INT Identity(1,1) Primary Key	Not Null, --Identificar agenda do nutricionista(também acho que não deveria ter).(chave primaria)
+	id_usuario				INT								Not Null, --Id da(o) nutricionista.
+	id_endereco				INT								Not Null, --id do endereço do nutricionista.
 	dt_inicio				Date							Not Null, --Data inicio do agendamento.
 	dt_fim					Date							Not Null, --Data fim do agendamento.
-	Preco					Float							Not Null, --Valores da consulta.
-	Forma_pagamento			Int								Not Null, --Os tipos de pagamentos que o tal nutricionista aceita.
-	Disponibilidade			Char(1)							Not Null, -- S - SIM ou N - NãO
-	Constraint FK_id_nutri_nutricionista_tb2 Foreign Key (id_nutri) References nutricionista_tb (id_nutri), --Chave estrangeira
+	Constraint FK_id_usuario_usuario_tb2 Foreign Key (id_usuario) References usuario_tb (id_usuario), --Chave estrangeira
 	Constraint FK_id_endereco_endereco_tb Foreign Key (id_endereco) References endereco_tb (id_endereco) --Chave estrangeira
 )
 
---Criação da tabela de consultas
-Create Table consulta_tb (
-	id_consulta				Int Identity(1,1) Primary Key	Not Null, --Identificar consulta que está sendo feita(chave primaria).
-	id_nutri				Int								Not Null, --Id da(o) nutricionista.
-	id_paciente				Int								Not Null, --Id do paciente.
-	id_agenda				Int								Not Null, --Id para indetificar a consulta do paciente e nutricionista. 
-	Status					Int								Not Null, --Se a consulta foi realizada, cancelada...
-	Constraint FK_id_nutri_nutricionista_tb3 Foreign Key (id_nutri) References nutricionista_tb (id_nutri), --Chave estrangeira
-	Constraint FK_id_paciente_paciente_tb Foreign Key (id_paciente) References paciente_tb (id_paciente), --Chave estrangeira
-	Constraint FK_id_agenda_agenda_tb Foreign Key (id_agenda) References agenda_tb (id_agenda) --Chave estrangeira
-)
 
---Criação da tabela da ficha do paciente.
-Create Table ficha_tb (
-	id_ficha				Int Identity(1,1) Primary Key	Not Null, --Identificar ficha do paciente(chave primaria).
-	id_nutri				Int								Not Null, --Id da(o) nutricionista.
-	id_paciente				Int								Not Null, --Id do paciente.
-	dt_cadastro				Date							Not Null, --data que foi realizada o cadastro.
-	Arquivo					Varbinary(Max)					Not Null, --é a mesma coisa que BLOB. 
-	Constraint FK_id_nutri_nutricionista_tb4 Foreign Key (id_nutri) References nutricionista_tb (id_nutri), --Chave estrangeira
-	Constraint FK_id_paciente_paciente_tb2 Foreign Key (id_paciente) References paciente_tb (id_paciente) --Chave estrangeira
-)
+
 
 
 
