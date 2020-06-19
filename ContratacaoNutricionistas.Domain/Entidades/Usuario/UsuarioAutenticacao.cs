404e384d8371bef4941b52a072fd56f9713111be
Pedro Henrique Pires <pedrohenriquepires13@gmail.com>
2020-06-19 17:58:49 -0300

    Pedro Henrique Pires - 19/06/2020 - Adicionando usuário de autenticação ignorado.

---
+++ ContratacaoNutricionistas.Domain/Entidades/Usuario/UsuarioAutenticacao.cs
@@ -0,0 +1,30 @@
+﻿using ContratacaoNutricionistas.Domain.Enumerados.Usuario;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContratacaoNutricionistas.Domain.Entidades.Usuario
+{
+    public class UsuarioAutenticacao
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Login
+        /// </summary>
+        public string Login { get; set; }
+
+        /// <summary>
+        /// Senha
+        /// </summary>
+        public string Senha { get; set; }
+
+        /// <summary>
+        /// Tipo de usuário
+        /// </summary>
+        public TipoUsuarioEnum TipoUsuario { get; set; }
+    }
+}
